d78464b53a837f12747fab21c55beab953f26720
FernandoKle <52257246+FernandoKle@users.noreply.github.com>
2022-11-05 02:09:48 +0000

    Update CDMA-ejemplo-python.py
    funciona copiando y pegando en el interprete, pero no hace las graficas desde el script. (Sigue sin estar completo)

--- CDMA-ejemplo-python.py
+++ CDMA-ejemplo-python.py
@@ -19,7 +19,7 @@ PN = np.random.randint(2, size=(n_PN,n))
 #print(PN)
 
 #muestra los datos de cada usuario
-for i in range(0,n):
+for i in range(n):
     print("\ndatos del usuario",i)
     print(datos[:,i].transpose())
     print("Secuencia pseudo aleatoria del usuario",i)
@@ -42,24 +42,35 @@ PN_plot = np.repeat(PN_long,100,1)
 data_plot = np.repeat(datos,100,1) 
 signal_plot = np.repeat(signal,100,1)
 
-L = PN_plot.size
+L = PN_plot.size #no se usa ?
 
-for i in range(0,n):
+for i in range(n):
     fig = plt.figure() #genera la figura
     
-    #x1 = np.linspace(0, data_plot[:,i].size , 3 ) #define al eje X (inicio, fin, muestras )
     ax1 = fig.add_subplot(3,1,1) #agrega el subplot, ax por axys (eje)
-    ax1.set_title("Datos")
+    ax1.set_title("Datos, usuario: " + str(i) )
     ax1.plot(data_plot[:,i])
     
-    #x2 = np.linspace(0, PN_plot[:,i].size , 3 )
     ax2 = fig.add_subplot(3,1,2) #agrega otro subplot
     ax2.set_title("Secuencia pseudo aleatoria")
     ax2.plot(PN_plot[:,i])
     
-    #x3 = np.linspace(0, signal_plot[:,i].size , 3 )
     ax3 = fig.add_subplot(3,1,3) #agrega otro subplot
     ax3.set_title("Señal codificada")
     ax3.plot(signal_plot[:,i])
     
     fig.show()
+
+#combinacion de señales
+
+senial_transmisora = signal.sum(axis=1) #sum(signal, 2)
+senial_transmisora_plot = signal_plot.sum(axis=1) #sum(signal, 2)
+#print(signal)
+#print(type(signal))
+#print(senial_transmisora)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(3,1,1)
+ax1.plot(senial_transmisora_plot ,linewidth=3)
+ax1.set_title("Señal a transmitir" )
+fig.show()
